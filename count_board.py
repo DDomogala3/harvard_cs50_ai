5f3e69642ab3c0c3a3b71cdf8d54e0a1c0fe487d
DDomogala3 <ddd38@nau.edu>
2025-08-06 20:32:55 -0500

    added_count

--- count_board.py
+++ count_board.py
@@ -10,7 +10,19 @@ board2 = [[EMPTY, "X", EMPTY],
 board3 = [[EMPTY, "X", "O"],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
-
+x_count = 0
+o_count = 0
+empty_count = 0
+for row in board3:
+    for element in row:
+        print(element)
+        if element == X:
+            x_count += 1
+        elif element == O:
+            o_count += 1
+        elif element == 'EMPTY':
+            empty_count += 1
+print(x_count, o_count, empty_count)
 def count_board(board):
     x_count = 0
     o_count = 0
