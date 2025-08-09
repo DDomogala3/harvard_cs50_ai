4c9097c9451d98be95886f9425b41ebeb85bc773
DDomogala3 <ddd38@nau.edu>
2025-08-09 16:23:24 -0500

    updated measuring function

--- count_board.py
+++ count_board.py
@@ -4,16 +4,19 @@ O = "O"
 board1 = [[EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
-board2 = [[EMPTY, "X", EMPTY],
-            [EMPTY, EMPTY, EMPTY],
+board2 = [[EMPTY, "X", "O",
+           EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
 board3 = [[EMPTY, "X", "O"],
             [EMPTY, EMPTY, EMPTY],
             [EMPTY, EMPTY, EMPTY]]
+board4 = [["X", "X", "O"],
+            ["O", "X", "O"],
+            ["X", "X", "O"]]
 x_count = 0
 o_count = 0
 empty_count = 0
-for row in board3:
+for row in board4:
     for element in row:
         print(element)
         if element == X:
@@ -22,7 +25,16 @@ for row in board3:
             o_count += 1
         elif element == 'EMPTY':
             empty_count += 1
-print(x_count, o_count, empty_count)
+if x_count > o_count:
+    print("O is next")
+elif o_count > x_count:
+    print("X is next")
+elif x_count == o_count & x_count + o_count < 9:
+    print("X goes next, board count is %d" % (x_count + o_count))
+elif x_count + o_count == 9:
+    print("Game over")
+
+
 def count_board(board):
     x_count = 0
     o_count = 0
