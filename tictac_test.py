43f9f14fbaa5f692128accebe68f954b5520f509
DDomogala3 <ddd38@nau.edu>
2025-10-17 14:04:22 -0500

    added tests
    added tests of the check value function

--- tictac_test.py
+++ tictac_test.py
@@ -26,29 +26,22 @@ print("This is the next move %s." % player(board1))
 player(initial_state())
 actions(board4)
 print(result(board5,(2,1)))
-print(winner(board4))
-print(winner(board5))
+
 top_left = (0,0)
 middle = (1,1)
 bottom_left = (2,2)
-print(check_value(board4,top_left)[0])
-print(check_value(board4,middle)[0])
-print(check_value(board4,bottom_left)[0])
-diagonal_list = []
-diagonal_list.append(check_value(board4,top_left))
-diagonal_list.append(check_value(board4, middle))
-diagonal_list.append(check_value(board4, bottom_left))
-print(diagonal_list)
+
+
+
 diagonal_x = 0
 diagonal_o = 0
-for i in diagonal_list:
-    diagonal_x += i[0]
-    diagonal_o += i[1]
-print(diagonal_x)
-print(diagonal_o)
-diagonal_list = []
-diagonal_score_x = 0
-diagonal_score_o = 0
-#assess_instance = Assess(board4, top_left, diagonal_list, diagonal_score_x, diagonal_score_o)
-assess_instance = Assess()
-print(assess_instance.check_value(board4,top_left))
+#for i in diagonal_list:
+#    diagonal_x += i[0]
+#    diagonal_o += i[1]
+#print(diagonal_x)
+#print(diagonal_o)
+#testing
+
+
+diagonal_list_test = [top_left,middle,bottom_left]
+print(check_value(board4, diagonal_list_test))
